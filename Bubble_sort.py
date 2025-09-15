4a2b06b6e7561f3fa28e80c0328fe037489bad29
Rashedul Kabir <49448196+Rashedul-Kabir@users.noreply.github.com>
2025-09-15 23:50:02 +0600

    Add files via upload

---
+++ Bubble_sort.py
@@ -0,0 +1,37 @@
+def bubble_sort(arr):
+    n = len(arr)
+    # Traverse through all array elements
+    for i in range(n):
+        # Last i elements are already sorted
+        for j in range(0, n-i-1):
+            # Traverse the array from 0 to n-i-1
+            # Swap if the element found is greater than the next element
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+
+nums = [64, 34, 25, 12, 22, 11, 90]
+print("Original:", nums)
+sorted_nums = bubble_sort(nums)
+print("Sorted:", sorted_nums)
+
+"""
+    Sorts an array using the bubble sort algorithm.
+
+    Parameters:
+    arr (list): The list of elements to be sorted.
+
+    Returns:
+    list: The sorted list.
+    """
+
+
+
+
+
+
+
+
+
+                                        
